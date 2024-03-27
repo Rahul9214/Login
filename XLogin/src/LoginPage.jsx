14b86706e32d9dc64746d71cf2aeb11507dd3cfb
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2024-03-28 01:56:42 +0530

    first commit

---
+++ XLogin/src/LoginPage.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+function LoginPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // Function to Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Simple validation for demonstration purposes
+        if (username === "user" && password === "password") {
+            setError("");
+            setIsSubmitted(true);
+        } else {
+            setError("Invalid username or password");
+            setIsSubmitted(false);
+        }
+    };
+
+    return (
+        <div>
+           <h1>Login Page</h1>
+           {isSubmitted ? (
+            <div>
+              <p>Welcome, {username}!</p>
+            </div>
+         ) : (
+            <form onSubmit={handleSubmit}>
+              {error && <p className="error">{error}</p>}
+              <div>
+                 <label htmlFor="username">Username:</label>
+                 <input 
+                   type="text"
+                   id="username"
+                   placeholder="username"
+                   value={username}
+                   onChange={(e) => setUsername(e.target.value)}
+                   required
+                 />
+              </div>
+
+              <div>
+                 <label htmlFor="passwoord">Password:</label>
+                 <input 
+                   type="password"
+                   id="password"
+                   placeholder="password"
+                   value={password}
+                   onChange={(e) => setPassword(e.target.value)}
+                   required
+                 />
+              </div>
+
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+           )}
+        </div>
+    );
+}
+
+export default LoginPage;
